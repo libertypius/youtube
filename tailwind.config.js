873c294bf04c226d0204c6b47c39312d12c055df
libertypius <libertypius40@gmail.com>
2026-03-16 06:18:28 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+          header:["Audiowide"],
+          home:["Autour"]
+      },
+      colors:{
+        cele:[""]
+      }
+    },
+  },
+  plugins: [],
+}
